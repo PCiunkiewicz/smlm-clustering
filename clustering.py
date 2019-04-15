a43d9d64bb944dcb286699de0945b52dfd48288c
Philip Ciunkiewicz <46083123+PCiunkiewicz@users.noreply.github.com>
2019-04-14 19:42:12 -0600

    More DBSCAN work, implemented custom random search

--- clustering.py
+++ clustering.py
@@ -1,83 +1,118 @@
+
+###############################################################################
+################################### Imports ###################################
+###############################################################################
+
+
 import sklearn
 from sklearn import metrics
 import pandas as pd
 import numpy as np
+from tqdm import trange
 from scipy.spatial.distance import cdist
 from time import time
 import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_score
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import ParameterSampler
 import seaborn as sns
 sns.set()
 
 
-def dbscan_verbose(db, XY, plot=False):
+###############################################################################
+################################## Functions ##################################
+###############################################################################
+
+
+def dbscan_verbose(db, X, plot=False,n=None,silent=False):
+    plt.close()
     core = np.zeros_like(db.labels_, dtype=bool)
     core[db.core_sample_indices_] = True
     labels = db.labels_
 
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    n_noise_ = list(labels).count(-1)
+    if n is None:
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
 
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print('Estimated number of noise points: %d' % n_noise_)
-    print('Total number of points: %d' % len(labels))
+        print(f'Estimated number of clusters: {n_clusters_}')
+        print(f'Estimated number of noise points: {n_noise_}')
+        print(f'Total number of points: {len(labels)}')
     
     if plot:
-        unique_labels = set(labels)
-        colors = [plt.cm.Spectral(each)
-                  for each in np.linspace(0, 1, len(unique_labels))]
+        plot_clusters(X,labels,core,n,silent)
         
-        for k, col in zip(unique_labels, colors):
-            cluster = (labels == k)
-            alpha = 0.4
-            ms = 6
-            if k == -1:
-                col = [0, 0, 0, 1]
-                alpha = 1
-                ms = 1
-
-            xy = XY[cluster & core]
-            plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                     markeredgewidth=0.0, markersize=ms, alpha=alpha)
-
-            xy = XY[cluster & ~core]
-            plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                     markeredgecolor='k', markersize=ms, alpha=alpha)
-
-        plt.show()
-        
-def custom_silhouette(dbscan,X):
-    samples = np.random.choice(np.arange(len(X)),10000)
-    xy = X[samples]
-    xydist = cdist(xy,xy)
-    db = dbscan.fit(X)
-    labels = db.labels_[samples]
-    score = silhouette_score(xydist, labels, metric="precomputed")
+def custom_silhouette(dbscan,X,cv=1):
+    scores = []
+    for i in range(cv):
+        samples = np.random.choice(np.arange(len(X)),10000)
+        xy = X[samples]
+        xydist = cdist(xy,xy)
+        db = dbscan.fit(X)
+        labels = db.labels_[samples]
+        scores.append(silhouette_score(xydist, labels, metric="precomputed"))
+    score = np.mean(scores)
+    stdev = np.std(scores)
+    if cv > 1:
+        return score,stdev
     
     return score
-
-def report(results, n_top=3):
-    for i in range(1, n_top + 1):
-        candidates = np.flatnonzero(results['rank_test_score'] == i)
-        for candidate in candidates:
-            print("Model with rank: {0}".format(i))
-            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
-                  results['mean_test_score'][candidate],
-                  results['std_test_score'][candidate]))
-            print("Parameters: {0}".format(results['params'][candidate]))
-            print("")
-            
-def param_search(dbscan,param_dist,X,n=10,cv=3,showreport=True):
-    random_search = RandomizedSearchCV(dbscan,param_distributions=param_dist,
-                                       n_iter=n,cv=cv,iid=False,
-                                       scoring=custom_silhouette)
-    start = time()
-    random_search.fit(X)
-    print("RandomizedSearchCV took %.2f seconds for %d candidate"
-          " parameter settings." % ((time() - start), n))
-    
-    if showreport:
-        report(random_search.cv_results_)
+         
+def random_search_custom(dbscan,param_dist,X,n=1,cv=5):
+    allparams = list(ParameterSampler(param_dist,n))
+    unique = list(set(frozenset(x.items()) for x in allparams))
+    params = [dict(x) for x in unique]
+    results = []
+    for i in trange(len(params)):
+        dbscan.set_params(**params[i])
+        params[i]['score'],params[i]['stdev'] = custom_silhouette(dbscan,X,cv=cv)
+        results.append(params[i])
         
-    return random_search
+    results = pd.DataFrame(results)
+    results.sort_values(by=['score'],ascending=False,inplace=True)
+    results = results[['score','stdev','eps','min_samples']]
+    
+    return results
+
+
+###############################################################################
+############################### Helper Functions ##############################
+###############################################################################
+
+
+def plot_clusters(X,labels,core,n=None,silent=False):
+    if n is not None:
+        mask = np.isin(labels,n)
+        X,labels,core = X[mask],labels[mask],core[mask]
+        
+    plotlabel = None
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+
+    for k, col in zip(unique_labels, colors):
+        cluster = (labels == k)
+        alpha = 0.4
+        ms = 6
+        if k == -1:
+            col = [0, 0, 0, 1]
+            alpha = 1
+            ms = 1
+        if n is not None:
+            plotlabel = str(k)
+            if not silent:
+                print(f'Cluster index: {k}')
+                print(f'Estimated number of points: {np.sum(cluster)}')
+                print(f'Estimated number of core points: {np.sum(cluster & core)}')
+                print(f'Estimated number of edge points: {np.sum(cluster & ~core)}\n')
+
+        xy = X[cluster & core]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgewidth=0.0, markersize=ms, alpha=alpha,
+                 label=plotlabel)
+
+        xy = X[cluster & ~core]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=ms, alpha=alpha)
+
+    if n is not None:
+        plt.legend()
+#     plt.show()
